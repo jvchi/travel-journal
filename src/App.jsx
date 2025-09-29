d5b2d8596ef7ba48cb00d3d07ab93c055b37d628
jvchi <elechidestinyj@gmail.com>
2025-09-29 12:37:14 +0100

    version1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "/Users/destiny/travel journal/src/components/Header.jsx"
+import Entry from "/Users/destiny/travel journal/src/components/Entry.jsx"
+import MountFuji from "/Users/destiny/travel journal/src/assets/japan.jpeg"
+export default function App(){
+  return(
+    <>
+      <Header/>
+      <main className="container">
+        <Entry
+          img={{src:MountFuji, alt:"Mount Fuji"}}
+          CountryName="Japan"
+          location="Mount Fuji"
+          GoogleMaps="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+          date="12 Jan, 2021 - 24 Jan, 2021"
+          about="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists"
+        />
+      </main>
+    </>
+  )
+}
---
+++ src/components/Entry.jsx
@@ -0,0 +1,21 @@
+import JapanImg from "/Users/destiny/travel journal/src/assets/japan.jpeg"
+import LocationMarker from "/Users/destiny/travel journal/src/assets/location.png"
+export default function Entry(props){
+  return(
+    <section className="journal">
+      <span>
+        <img src={props.img.src} alt={props.img.alt} className="japan-img"/>
+      </span>
+      <ul className="Destination">
+        <li className="country-info"><img src={LocationMarker} alt="location-marker" className="location-marker" /><span className="country-name">{props.CountryName}</span> <a href={props.GoogleMaps}className="maps">View on Google Maps</a></li>
+
+        <li className="mount-fuji">{props.location}</li>
+
+        <li className="date">{props.date}</li>
+
+        <li className="about">{props.about}</li>
+
+      </ul>
+    </section>
+  )
+}
