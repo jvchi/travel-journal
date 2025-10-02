13187602cafc2491d839a0dbb2b467b471b2a2ce
jvchi <elechidestinyj@gmail.com>
2025-10-02 17:18:27 +0100

    moved data to a new js file used js spread operator to get array.

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,13 +1,14 @@
 import JapanImg from "/Users/destiny/travel journal/src/assets/japan.jpeg"
 import LocationMarker from "/Users/destiny/travel journal/src/assets/location.png"
 export default function Entry(props){
+  console.log(props)
   return(
     <section className="journal">
       <span>
         <img src={props.img.src} alt={props.img.alt} className="japan-img"/>
       </span>
       <ul className="Destination">
-        <li className="country-info"><img src={LocationMarker} alt="location-marker" className="location-marker" /><span className="country-name">{props.CountryName}</span> <a href={props.GoogleMaps}className="maps">View on Google Maps</a></li>
+        <li className="country-info"><img src={LocationMarker} alt="location-marker" className="location-marker" /><span className="country-name">{props.CountryName}</span> <a href={props.GoogleMaps}>View on Google Maps</a></li>
 
         <li className="mount-fuji">{props.location}</li>
 
---
+++ src/components/data.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    id: 1,
+    img: {
+      src: "/src/assets/japan.jpeg",
+      alt: "Mount Fuji"
+    },
+    CountryName: "Japan",
+    location: "Mount Fuji",
+    GoogleMaps: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+    date: "12 Jan, 2021 - 24 Jan, 2021",
+    about: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+  },
+  {
+    id: 2,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-australia-image-url",
+      alt: "Sydney Opera House"
+    },
+    CountryName: "Australia",
+    location: "Sydney Opera House",
+    GoogleMaps: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+    date: "27 May, 2021 - 8 Jun, 2021",
+    about: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+  },
+  {
+    id: 3,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-norway-image-url",
+      alt: "Geirangerfjord"
+    },
+    CountryName: "Norway",
+    location: "Geirangerfjord",
+    GoogleMaps: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+    date: "01 Oct, 2021 - 18 Nov, 2021",
+    about: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+  },
+  {
+    id: 4,
+    img: {
+      src: "https://images.unsplash.com/photo-1679996437431-4fbd3cd1c9bf?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Pantjoran Chinatown PIK"
+    },
+    CountryName: "Indonesia",
+    location: "Pantjoran Chinatown PIK",
+    GoogleMaps: "https://maps.apple/p/hY6TfUw71cUCeZ",
+    date: "01 Oct, 2021 - 18 Nov, 2021",
+    about: "Pantjoran Chinatown PIK is a vibrant cultural and culinary spot in Jakarta, featuring Chinese-inspired architecture, food stalls, and lively photo-worthy streets."
+  },
+  {
+    id: 5,
+    img: {
+      src: "https://images.unsplash.com/photo-1590716209211-ea74d5f63573?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Pantjoran Chinatown PIK"
+    },
+    CountryName: "Australia",
+    location: "Sydney Opera House",
+    GoogleMaps: "https://maps.app.goo.gl/sRwzHHyWTrPyjSRB8?g_st=ipc",
+    date: "01 Oct, 2021 - 18 Nov, 2021",
+    about: "The Sydney Opera House is a world-famous performing arts center on Sydney Harbour, recognized for its iconic sail-like design and status as a UNESCO World Heritage Site."
+  },
+  {
+    id: 6,
+    img: {
+      src: "https://images.unsplash.com/photo-1684227291660-8b8f99780b10?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "The Elizabeth Tower"
+    },
+    CountryName: "United Kingdom",
+    location: "The Elizabeth Tower",
+    GoogleMaps: "https://maps.app.goo.gl/o88cU5rbmPU2Q547A?g_st=ipc",
+    date: "May 16, 2023",
+    about: "The Elizabeth Tower, commonly called Big Ben, is a landmark clock tower at the north end of the Palace of Westminster in London, known for its iconic chimes."
+  }
+]
